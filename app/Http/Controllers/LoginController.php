0ce49e253d74ac6b7a4241ccc4ecdd66d9e2dfb0
rizqiharfian <jayarayak99@gmail.com>
2025-01-24 21:28:58 +0700

    [update LoginController]

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -38,6 +38,6 @@ class LoginController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('produk');
+        return redirect()->route('login');
     }
 }
